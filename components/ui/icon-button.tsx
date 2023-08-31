d3818d445e6fba79cc6295f76a51e4f0b596432f
Ali Mohammadinia <alimohammadinia3141@gmail.com>
2023-08-31 14:11:03 +0330

    complete card products

---
+++ components/ui/icon-button.tsx
@@ -0,0 +1,24 @@
+import {cn} from "@/lib/utils";
+import {MouseEventHandler} from "react";
+
+interface IconButtonProps {
+    onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
+    icon: React.ReactElement;
+    className?: string
+}
+
+const IconButton: React.FC<IconButtonProps> = ({ onClick, icon, className}) => {
+    return (
+        <button
+            onClick={onClick}
+            className={cn(
+                "rounded-full flex justify-center bg-white border shadow-md p-2 hover:scale-110 transition",
+                className
+            )}
+        >
+
+        </button>
+    );
+};
+
+export default IconButton;
